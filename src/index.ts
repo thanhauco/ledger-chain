667388e045591a4118fba7f270217d986f3e7a3b
Thanh Vu <thanhauco@gmail.com>
2021-01-10 15:46:00 -0800

    Test Chain in Main

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Ledger Chain');
+import { Blockchain } from './Blockchain';
+import { Block } from './Block';
+const coin = new Blockchain();
+coin.addBlock(new Block(1, Date.now(), 'Tx 1'));
+coin.addBlock(new Block(2, Date.now(), 'Tx 2'));
+console.log(JSON.stringify(coin, null, 2));
