b4c63c73252c26bb4c759345e7e40b0c67c63211
Thanh Vu <thanhauco@gmail.com>
2021-01-16 10:28:00 -0800

    Add Nonce to Block

--- src/Block.ts
+++ src/Block.ts
@@ -5,14 +5,23 @@ export class Block {
     public data: string;
     public previousHash: string;
     public hash: string;
+    public nonce: number;
     constructor(index: number, timestamp: number, data: string, previousHash: string = '') {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
     calculateHash(): string {
-        return CryptoJS.SHA256(this.index + this.previousHash + this.timestamp + this.data).toString();
+        return CryptoJS.SHA256(this.index + this.previousHash + this.timestamp + this.data + this.nonce).toString();
+    }
+    mineBlock(difficulty: number) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log('Block mined: ' + this.hash);
     }
 }
