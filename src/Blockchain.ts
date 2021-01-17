304a9b2fcd21edb64f2eaea5ad129a38b73eddb8
Thanh Vu <thanhauco@gmail.com>
2021-01-17 13:28:00 -0800

    Add Chain Validation

--- src/Blockchain.ts
+++ src/Blockchain.ts
@@ -17,4 +17,13 @@ export class Blockchain {
         newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
+    isChainValid(): boolean {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+            if (currentBlock.hash !== currentBlock.calculateHash()) return false;
+            if (currentBlock.previousHash !== previousBlock.hash) return false;
+        }
+        return true;
+    }
 }
