1988eb7a1299a33fe7d2a138352c7d04a3bb7549
Thanh Vu <thanhauco@gmail.com>
2021-01-19 00:37:00 -0800

    Test Validation

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { Blockchain } from './Blockchain';
 import { Block } from './Block';
 const coin = new Blockchain();
 coin.addBlock(new Block(1, Date.now(), 'Tx 1'));
-coin.addBlock(new Block(2, Date.now(), 'Tx 2'));
-console.log(JSON.stringify(coin, null, 2));
+console.log('Valid?', coin.isChainValid());
+coin.chain[1].data = 'Hacked';
+console.log('Valid?', coin.isChainValid());
