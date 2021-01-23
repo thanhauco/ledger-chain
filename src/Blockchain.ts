ee8621ca2044a97d0fab6ab6deffe8e9698f41ed
Thanh Vu <thanhauco@gmail.com>
2021-01-23 10:11:00 -0800

    Update Blockchain for Pending Tx

--- src/Blockchain.ts
+++ src/Blockchain.ts
@@ -1,29 +1,39 @@
 import { Block } from './Block';
+import { Transaction } from './Transaction';
 export class Blockchain {
     public chain: Block[];
     public difficulty: number;
+    public pendingTransactions: Transaction[];
+    public miningReward: number;
     constructor() {
         this.chain = [this.createGenesisBlock()];
         this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
     createGenesisBlock(): Block {
-        return new Block(0, Date.now(), 'Genesis Block', '0');
+        return new Block(Date.now(), [], '0');
     }
     getLatestBlock(): Block {
         return this.chain[this.chain.length - 1];
     }
-    addBlock(newBlock: Block) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress: string) {
+        const block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+        block.mineBlock(this.difficulty);
+        this.chain.push(block);
+        this.pendingTransactions = [new Transaction(null, miningRewardAddress, this.miningReward)];
     }
-    isChainValid(): boolean {
-        for (let i = 1; i < this.chain.length; i++) {
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-            if (currentBlock.hash !== currentBlock.calculateHash()) return false;
-            if (currentBlock.previousHash !== previousBlock.hash) return false;
+    createTransaction(transaction: Transaction) {
+        this.pendingTransactions.push(transaction);
+    }
+    getBalanceOfAddress(address: string): number {
+        let balance = 0;
+        for (const block of this.chain) {
+            for (const trans of block.transactions) {
+                if (trans.fromAddress === address) balance -= trans.amount;
+                if (trans.toAddress === address) balance += trans.amount;
+            }
         }
-        return true;
+        return balance;
     }
 }
