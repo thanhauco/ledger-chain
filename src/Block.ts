b774367a4e59148eeabeca032933b5e2f51551f4
Thanh Vu <thanhauco@gmail.com>
2021-01-26 01:44:00 -0800

    Update Block for Transactions

--- src/Block.ts
+++ src/Block.ts
@@ -1,21 +1,20 @@
 import * as CryptoJS from 'crypto-js';
+import { Transaction } from './Transaction';
 export class Block {
-    public index: number;
     public timestamp: number;
-    public data: string;
+    public transactions: Transaction[];
     public previousHash: string;
     public hash: string;
     public nonce: number;
-    constructor(index: number, timestamp: number, data: string, previousHash: string = '') {
-        this.index = index;
+    constructor(timestamp: number, transactions: Transaction[], previousHash: string = '') {
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
     }
     calculateHash(): string {
-        return CryptoJS.SHA256(this.index + this.previousHash + this.timestamp + this.data + this.nonce).toString();
+        return CryptoJS.SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
     }
     mineBlock(difficulty: number) {
         while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
