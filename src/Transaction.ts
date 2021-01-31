919ed5b3256786e4b9f23e1a04513274f23f8a88
Thanh Vu <thanhauco@gmail.com>
2021-01-31 15:23:00 -0800

    Sign Transactions

--- src/Transaction.ts
+++ src/Transaction.ts
@@ -1,3 +1,24 @@
+import * as CryptoJS from 'crypto-js';
+import * as EC from 'elliptic';
+const ec = new EC.ec('secp256k1');
 export class Transaction {
+    public signature: string;
     constructor(public fromAddress: string, public toAddress: string, public amount: number) {}
+    calculateHash(): string {
+        return CryptoJS.SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+    }
+    signTransaction(signingKey: any) {
+        if (signingKey.getPublic('hex') !== this.fromAddress) {
+            throw new Error('You cannot sign transactions for other wallets!');
+        }
+        const hashTx = this.calculateHash();
+        const sig = signingKey.sign(hashTx, 'base64');
+        this.signature = sig.toDER('hex');
+    }
+    isValid(): boolean {
+        if (this.fromAddress === null) return true;
+        if (!this.signature || this.signature.length === 0) throw new Error('No signature in this transaction');
+        const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+        return publicKey.verify(this.calculateHash(), this.signature);
+    }
 }
